d3544a28c531dc1d244fcfeac60fd55bc323efff
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-04-22 16:03:34 -0700

    Added most styling to bottom text content

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -3,10 +3,10 @@ import './Card.css';
 
 const CardContent = () => {
     return (
-        <div class="CardContent">
+        <div class="card-content">
         <h2 class="CardTitle">Get Started with React</h2>
         <p class="CardText">React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
-        <p class="CardLink">reactjs.org</p>
+        <p class="card-link">reactjs.org</p>
         </div>
     )
 }
