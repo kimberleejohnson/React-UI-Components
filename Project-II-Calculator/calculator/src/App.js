8f90875e4d90625d62c1859e717c91f7e70c47d8
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-04-23 12:41:23 -0700

    Added top display

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Display from './components/DisplayComponents/CalculatorDisplay';
 
 const App = () => {
   return (
     <div className="calc-container">
-      Test!
+      <Display /> 
     </div>
   );
 };
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Display.css';
+
+const Display = () => {
+  return (
+    <div className="display-container">
+      <p>0</p>
+    </div>
+  );
+};
+
+export default Display;
