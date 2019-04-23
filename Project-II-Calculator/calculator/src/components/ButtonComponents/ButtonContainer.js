0846348f12cd9cf6558e4f016cfcbeac372e2df3
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-04-23 13:14:31 -0700

    Added numbers container

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ButtonContainer.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ButtonContainer.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import ActionButton from './ActionButton'; 
+import NumbersContainer from './NumbersContainer'; 
+
+import './Button.css';
 
 const ButtonContainer = () => {
     return (
       <div className="button-container">
         <ActionButton />
+        <NumbersContainer /> 
         <ActionButton />  
       </div>
     );
---
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumbersContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Button.css';
+
+const NumbersContainer = () => {
+    return (
+      <div className="numbers-container">
+        <p>Test!</p>
+      </div>
+    );
+  };
+  
+  export default NumbersContainer;
+  
